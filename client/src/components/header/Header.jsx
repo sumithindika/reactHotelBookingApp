8823c9881a05f9f1fd2a2ce4817ebe08fa316620
Sumith Indika Weerasingh <tsumith44@gmail.com>
2022-08-31 10:39:16 +0530

    searchBar
    header search

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./header.css";
-
+import {faBed,faCalendarDays,faPerson} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Calendar, DateRange} from  "react-date-range";
 
 const Header = () => {
+
+// const {Date,setDate} =useState =>{
+
+
+// }
+
+
   return (
+    <div className='header'>
     <div className="headerSearch">
-    <div className="headerSearchItem"></div>
+      <div className="headerSearchItem">
+    <FontAwesomeIcon icon={faBed} className='headerIcon' />
+ <input 
+   type="text" 
+  placeholder='where are you going ?'
+   className='headerSearchInput' 
+
+ />
+
+    </div>
+    <div className="headerSearchItem">
+    <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+<span className='headerSearchText'>Date to Date</span>
+
+    </div>
+
+
+    <div className="headerSearchItem">
+    <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+    <span className='headerSearchText'>1 Adults 0 Children 1 Rooms</span>
+
+    </div>
+    <div className="headerSearchItem">
+    <button className="headerBtn">Search</button>
+
+    </div>
+
+    </div>
     </div>
   )
 }
